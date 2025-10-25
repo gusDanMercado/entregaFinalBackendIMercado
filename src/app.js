3535e2eb3a2d8871bd62681c866d3c89b7a00327
gusDanMercado <gustavomerk2@gmail.com>
2025-10-24 21:33:01 -0300

    actualizacion de mi proyecto

--- src/app.js
+++ src/app.js
@@ -6,3 +6,9 @@ mongoose
   .connect(MONGO_URI)
   .then(() => console.log("Conectado a MongoDB"))
   .catch((err) => console.error("Error de conexión:", err));
+
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => res.send("Servidor funcionando OK"));
+app.listen(8081, () => console.log("Servidor en puerto 8081"));
